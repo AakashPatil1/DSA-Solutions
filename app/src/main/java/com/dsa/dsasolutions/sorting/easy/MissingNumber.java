01f2843e8c9eaa77beb65fca1ee6895e3c60b360
AakashPatil1 <aakashap1818@gmail.com>
2023-12-18 21:03:12 +0530

    missingNumber

---
+++ app/src/main/java/com/dsa/dsasolutions/sorting/easy/MissingNumber.java
@@ -0,0 +1,18 @@
+package com.dsa.dsasolutions.sorting.easy;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] num = {3, 0, 1};
+        System.out.println(missingNumber(num));
+    }
+
+
+    public static int missingNumber(int[] nums) {
+        int ans = nums.length;
+
+        for (int i = 0; i < nums.length; ++i)
+            ans ^= i ^ nums[i];
+
+        return ans;
+    }
+}
