763478722143334cb2d8113eb1bb06daa639f132
AakashPatil1 <aakashap1818@gmail.com>
2023-12-19 21:05:05 +0530

    sortByBits

---
+++ app/src/main/java/com/dsa/dsasolutions/sorting/easy/SortByBits.java
@@ -0,0 +1,20 @@
+package com.dsa.dsasolutions.sorting.easy;
+
+import java.util.Arrays;
+
+public class SortByBits {
+    public static void main(String[] args) {
+        int[] num = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(Arrays.toString(sortByBits(num)));
+    }
+
+    public static int[] sortByBits(int[] arr) {
+        Integer[] A = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(A,
+                (a, b)
+                        -> Integer.bitCount(a) == Integer.bitCount(b)
+                        ? a - b
+                        : Integer.bitCount(a) - Integer.bitCount(b));
+        return Arrays.stream(A).mapToInt(Integer::intValue).toArray();
+    }
+}
