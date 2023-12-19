a8774f2797d4ec0a9058f85ae0f12c795b7d49eb
AakashPatil1 <aakashap1818@gmail.com>
2023-12-19 21:01:54 +0530

    arrayRankTransform

---
+++ app/src/main/java/com/dsa/dsasolutions/sorting/easy/ArrayRankTransform.java
@@ -0,0 +1,27 @@
+package com.dsa.dsasolutions.sorting.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayRankTransform {
+    public static void main(String[] args) {
+        int[] num = {40,10,20,30};
+        System.out.println(Arrays.toString(arrayRankTransform(num)));
+    }
+
+    public static int[] arrayRankTransform(int[] arr) {
+        int[] sortedArr = arr.clone();
+        Map<Integer, Integer> rank = new HashMap<>();
+
+        Arrays.sort(sortedArr);
+
+        for (final int a : sortedArr)
+            rank.putIfAbsent(a, rank.size() + 1);
+
+        for (int i = 0; i < arr.length; ++i)
+            arr[i] = rank.get(arr[i]);
+
+        return arr;
+    }
+}
