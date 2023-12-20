b006f812dbf0f01407f3fb8e0340952606d018af
AakashPatil1 <aakashap1818@gmail.com>
2023-12-20 20:42:31 +0530

    twoSum

---
+++ app/src/main/java/com/dsa/dsasolutions/sorting/easy/TwoSum.java
@@ -0,0 +1,27 @@
+package com.dsa.dsasolutions.sorting.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] num = {2,7,11,15};
+        int target =9;
+        System.out.println(Arrays.toString(twoSum(num,target)));
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (numToIndex.containsKey(target - nums[i]))
+                return new int[] {numToIndex.get(target - nums[i]), i};
+            numToIndex.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException();
+    }
+}
